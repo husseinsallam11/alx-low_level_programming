f3b2d5eb7c705573c9b813b3f1616e38e76a6909
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-16 18:53:01 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - function print sentence to standered error
  * Return: 1
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
