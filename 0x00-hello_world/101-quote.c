e372b9db4bf6c31d0dd8215193912b843361c369
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-16 18:56:15 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - function print sentence to standered error
- * Return: 1
+ *main - prints exactly "and that piece of art is usefu Dora Korpar, 2015-10-19",
+ *followed by a new line, to the standard error.
+ *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
