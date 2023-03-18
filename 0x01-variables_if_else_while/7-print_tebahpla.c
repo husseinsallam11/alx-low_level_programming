da4b67f8fc898bb1a04f0d8b19ec9f80c4ae639a
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-17 19:26:59 -0700

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main print lower care alphabet in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch=z
+
+	for (ch = z ; ch >= a ; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
