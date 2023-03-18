d485912a0a8a33d2feafa77bf6cc52bfa6396660
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-17 19:20:12 -0700

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for(num = 0 ; num <=9 ; num++)
+		putchar(num);
+	purchar('\n');
+	return(0);
+}
+
