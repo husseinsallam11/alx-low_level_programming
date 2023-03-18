2861e3e026e6932d8e7b08315c231e2efd38a880
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-17 18:55:50 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ *  * main - print all aplhabet letters except q and e     
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		++ch;
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
