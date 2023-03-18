856259bc38f4bd3cf72459e4f3db7bb6c2f7d192
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-17 18:45:21 -0700

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,18 @@
 
 int main(void)
 {
-	char ch ='a';
+	char ch = 'a';
 
-	char CH='A';
+	char CH = 'A';
 
-		for(ch='a';ch<='z';ch++)
+		for(ch = 'a'; ch<='z'; ch++)
 		{
 			putchar(ch);
 		}
-		for(CH='A';CH<='Z';CH++)
+		for(CH = 'A'; CH <= 'Z'; CH++)
 		{
 			putchar(CH);
 		}
 	return (0);
+
 }
