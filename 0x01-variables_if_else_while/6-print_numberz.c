3677b58fb2f2d04b4c1ee0f98702f7653142d674
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-17 19:22:27 -0700

    update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int num = 0;
 
-	for(num = 0 ; num <=9 ; num++)
+	for (num = 0 ; num <= 9 ; num++)
 		putchar(num);
-	putchar('\n');
-	return(0);
+	puichar('\n');
+	return (0);
 }
 
