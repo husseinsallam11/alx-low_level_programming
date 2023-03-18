b159f0739dc0ec0df04082aac493a1525db4f359
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-17 18:31:46 -0700

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	int n,digit;
+	int n, digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	digit = n % 10;
+	digit = n % 10; /*gets last digit*/
 
 		if (digit > 5)
 			printf("Last digit of %i is %i and is greater than 5\n", n, digit);
