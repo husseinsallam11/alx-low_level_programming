bbe213af8940a1c9e024639ca4bfe4a2def4e289
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-17 19:00:39 -0700

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,12 @@
 int main(void)
 {
 	char ch = 'a';
-
-	while (ch <= 'z')
+	while(ch <= 'z')
 	{
-		if (ch == 'e' || ch == 'q')
-		++ch;
-		putchar(ch);
-		++ch;
+		if(ch == 'e' || ch == 'q')
+			++ch;
+	putchar(ch);
+	++ch;
 	}
 	putchar('\n');
 
