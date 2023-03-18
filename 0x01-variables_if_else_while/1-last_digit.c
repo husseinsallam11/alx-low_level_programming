96dd285479454c909e1d015ac109f516bbd8b2fc
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-17 18:18:37 -0700

    update

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints n value wheter its greater 5/smalle 6/equal
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10; /*gets last digit*/
+
+		if (digit > 5)
+			printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+		else if (digit == 0)
+			printf("Last digit of %i is %i and is 0\n", n, digit);
+		else if (digit < 6 && digit != 0)
+			printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+	return (0);
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphapets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch ='a';
+	char CH='A';
+		for(ch='a':ch<='z':ch++)
+		{
+			putchar(ch);
+		}
+		for(CH='A':CH<='Z':CH++)
+		{
+			putchar(CH);
+	return 0;
