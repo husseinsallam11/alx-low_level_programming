945ea24fdef5b7ccef70afa2c773aa195189e8d0
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-17 19:12:01 -0700

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits of base ten from zero
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num=0 ; num <=9 ; num++)
+		printf (%i , num)
+	printf('\n')
+	
+	return (0);
+}
