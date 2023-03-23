16bf1946d447b5bfe086802dd128bf7be09d9abe
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-23 12:40:09 -0700

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ *  _isupper - checks if c is uppercase
+ *  @c: input
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+
+int -isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
