37ee524c6cc07765586183b7d6c4ace7fa18ddd7
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-23 12:53:56 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int one
+ * @b: int two
+ * Return: the result of one x two
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
