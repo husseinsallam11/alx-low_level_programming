c2becfa34692f77d5f2fe77b4f702d8538f218be
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-03-27 16:16:57 -0700

    update

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy strings pinted to by src
+ * @dest: pointer to buffer
+ * @src: string to be copied
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+
