91b6cfed785f2d7d37b67b7270a80019c01aa49b
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-04-15 13:46:19 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - program that prints its name
+ * @argc: no of arg
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc , char* argv[])
+{
+	printf("argv = %s/n" , *argv);
+
+	return(0);
+}
