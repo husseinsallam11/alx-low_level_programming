19916959a5f559dea9ce42913bbbcd343836cfbc
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-04-15 14:04:07 -0700

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints no of argument
+ * @argc: no of arg
+ * @argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d" , argc);
+
+	return (0);
+}
