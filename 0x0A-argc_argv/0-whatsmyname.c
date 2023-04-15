e003979401bedec6067d70b76924280a19b9e0b7
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-04-15 13:59:03 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: 0
  */
 
-int main(int argc __attribute__((unused)) , char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n" , *argv);
+	printf("%s\n", *argv);
 
-	return(0);
+	return (0);
 }
