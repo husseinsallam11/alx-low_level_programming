cfa7d4fe81e50e3e17fade8a966ec0530b7ed1df
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-04-15 14:18:36 -0700

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,14 +8,13 @@
  *
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n" , argv[i]);
+		printf("%s\n", argv[i]);
 	}
 
 	return (0);
