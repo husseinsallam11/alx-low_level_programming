32c267c34ff6374efced0b4629503728cd4f18e7
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-05-02 13:02:05 -0700

    update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *pointy = h;
+	size_t c = 0;
+
+	while (pointy != NULL)
+	{
+		printf("%d\n", pointy->n);
+		c += 1;
+		pointy = pointy->next;
+	}
+	return (count);
+}
