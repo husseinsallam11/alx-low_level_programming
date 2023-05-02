775768c30c7e33042484cc18db09fc48a71b73ba
husseinsallam11git config --global user.name husseinsallam11 <hm25030@gmail.com>
2023-05-02 14:34:04 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - prints all the elements of a linked list
@@ -8,15 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *temp;
 	unsigned int count = 0;
 
-	temp = h;
-	while (temp)
+	while (h)
 	{
-		printf("%d\n", temp->n);
+		printf("%d\n", h->n);
 		count++;
-		temp = temp->next;
+		h = h->next;
 	}
-	return (count);
+	return (h);
 }
